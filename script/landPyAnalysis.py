2f494bd66be0038b9ced047bbaf53c73b1ccdc0c
lux23@uw.edu <lux23@uw.edu>
2018-09-29 16:42:35 -0700

    1.0.0

---
+++ script/landPyAnalysis.py
@@ -0,0 +1,5 @@
+import geopandas as gpd
+import pandas as pd
+import os
+path = 'C:/Users/LXB19/Documents/LandUsePermitAnalysis/LandPermitAnalysis/data'
+data = pd.read_csv('C:/Users/LXB19/Documents/LandUsePermitAnalysis/LandPermitAnalysis/data',)
