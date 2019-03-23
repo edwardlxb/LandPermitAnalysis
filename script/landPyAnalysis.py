9292ce9731f787c382e4537c952af8c11ee1666d
edwardlxb <lux23@uw.edu>
2019-03-23 14:25:45 -0700

    v1.1 Cleaning

--- script/landPyAnalysis.py
+++
@@ -1,5 +0,0 @@
-import geopandas as gpd
-import pandas as pd
-import os
-path = 'C:/Users/LXB19/Documents/LandUsePermitAnalysis/LandPermitAnalysis/data'
-data = pd.read_csv('C:/Users/LXB19/Documents/LandUsePermitAnalysis/LandPermitAnalysis/data',)
